bbcd5276619e8f81f369a45fe2a8e965cf315cc2
yize <zgl.this@live.com>
2018-03-08 23:38:35 +0800

    Initial commit

---
+++ src/main/java/me/cauley/springcloudexample/SpringCloudExampleApplication.java
@@ -0,0 +1,12 @@
+package me.cauley.springcloudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudExampleApplication.class, args);
+	}
+}
